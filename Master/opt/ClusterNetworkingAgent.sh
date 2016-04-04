6429b41b406514ae6f10930e83337bf784ed09b8
Michael Telatynski <7t3chguy@gmail.com>
2016-04-04 07:15:25 +0100

    Fix Networking Agent

--- Master/opt/ClusterNetworkingAgent.sh
+++ Master/opt/ClusterNetworkingAgent.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 case "${ACTION}" in
-remove)
-        logger "Removing ${INTERFACE} to Pi Cluster Bridge (br0)"
-        brctl delif br0 ${INTERFACE}
-        ;;
-add)
-        logger "Adding ${INTERFACE} to Pi Cluster Bridge (br0)"
-        brctl addif br0 ${INTERFACE}
-        ;;
-*)
-        logger "Doing Nothing on ${INTERFACE} to Pi Cluster Bridge (br0) -- ${ACTION}"
-        ;;
+        remove)
+                logger "Removing ${INTERFACE} to Pi Cluster Bridge (br0)"
+                brctl delif br0 ${INTERFACE}
+                ;;
+        add)
+                logger "Adding ${INTERFACE} to Pi Cluster Bridge (br0)"
+                brctl addif br0 ${INTERFACE}
+                ;;
+        *)
+                logger "Doing Nothing on ${INTERFACE} to Pi Cluster Bridge (br0) -- ${ACTION}"
+                ;;
+esac
